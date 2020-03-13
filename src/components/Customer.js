1b00a4d7192499948995cb0051ee58019da69c46
aejeong <aejeong-context@kakao.com>
2020-03-13 17:42:24 +0900

    Metrial UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,43 +1,53 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     //항상 수행되는 내용 
     //실제 그려지는 내용
     render(){
         return(
+
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
             //div가 꼭 있어야 함.
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-               
-            </div>
+            // <div>
+            //     <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+            //     <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            // </div>
         )
 
     }
 }
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
+// class CustomerProfile extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <img src={this.props.image} alt="profile"/>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
+//         )
+//     }
+// }
 
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
+// class CustomerInfo extends React.Component{
+//     render(){
+//         return(
+//             <div>
               
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p> 
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
+//                 <p>{this.props.birthday}</p>
+//                 <p>{this.props.gender}</p> 
+//                 <p>{this.props.job}</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default Customer;
 
