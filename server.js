8a5b160c191531a68157e38138f92f1d1030ce6e
aejeong <aejeong-context@kakao.com>
2020-03-23 00:18:59 +0900

    DB+EXPRESS 연동

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-// app.get('/api/hello',(req,res)=>{
-//     res.send({message:'Hello Express!'});
 
-// });
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host:conf.host,
+  user:conf.user,
+  password:conf.password,
+  port:conf.port,
+  database:conf.database
+
+});
+
 app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-        'id':1,
-        'image':'https://placeimg.com/64/64/3',
-        'name':'신애정',
-        'birthday':'940605',
-        'gender':'여자',
-        'job':'무직'
-      },
-      {
-        'id':2,
-        'image':'https://placeimg.com/64/64/2',
-        'name':'양지수',
-        'birthday':'960910',
-        'gender':'남자',
-        'job':'무직'
-      }
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+        (err,rows,fields)=>{
+          res.send(rows);
+        }
       
-      ]);
+      );
 });
 app.listen(port,()=>console.log(`Listening on port ${port}`));
