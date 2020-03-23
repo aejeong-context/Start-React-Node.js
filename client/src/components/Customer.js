31d1244b5416cb5d35b57f97afb4652a9079c042
aejeong <aejeong-context@kakao.com>
2020-03-23 23:57:46 +0900

    Form 구현 값 받아오기

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -13,7 +13,6 @@ class Customer extends React.Component{
                 <TableCell><img src={this.props.image} alt="profile"/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
-                <TableCell>{this.props.gender}</TableCell>
                 <TableCell>{this.props.job}</TableCell>
             </TableRow>
             //div가 꼭 있어야 함.
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+//정보 받아와서
 const data = fs.readFileSync('./database.json');
+//json형태로 편환
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
+//database 연결부분
 const connection = mysql.createConnection({
   host:conf.host,
   user:conf.user,
@@ -24,6 +26,8 @@ app.get('/api/customers',(req,res)=>{
   connection.query(
     "SELECT * FROM CUSTOMER",
         (err,rows,fields)=>{
+          //배열형태로 가지고온다
+          //fields - 결과값 화면 출력 
           res.send(rows);
         }
       
